f9243b36e517574059d622e682805eccb6635d20
Jacob Inman <jrinman6295@gmail.com>
2025-01-15 08:35:40 -0800

    item objects initialize based off attributes, pushing branch back up

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Item do
       @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
       expect(@item1).to be_a Item
-      expect(@item2[:name]).to eq('Apple Pie (Slice)')
-      expect(@item2[:price]).to eq("$2.50")
+      expect(@item2.name).to eq('Apple Pie (Slice)')
+      expect(@item2.price).to eq("$2.50")
     end
   end
 end
