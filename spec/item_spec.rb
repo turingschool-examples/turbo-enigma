b12454bb518c4ab0072c8d7558fb094aed516b2e
Jacob Inman <jrinman6295@gmail.com>
2025-01-15 08:31:46 -0800

    item spec completed, beginning class

---
+++ spec/item_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require 'pry'
+require './lib/item'
+
+RSpec.describe Item do
+
+  describe 'initialize' do
+    it 'exists' do
+      @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      expect(@item1).to be_a Item
+      expect(@item2[:name]).to eq('Apple Pie (Slice)')
+      expect(@item2[:price]).to eq("$2.50")
+    end
+  end
+end
