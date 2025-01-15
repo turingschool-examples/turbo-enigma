cd0f82e87763d5bd06a1ead6e892299094c4fc8d
Sebastian McKee <sebmcke@gmail.com>
2025-01-15 10:22:35 -0600

    Add second check_stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -20,6 +20,12 @@ describe FoodTruck do
         it 'can check the stock of an out of stock item' do
             expect(@food_truck.check_stock(@item1)).to eq(0)
         end
+
+        it 'can check the stock of an in stock item' do
+            @food_truck.stock(@item1, 30)
+
+            expect(@food_truck.check_stock(@item1)).to eq(30)
+        end
     end
 
     describe '#stock' do
