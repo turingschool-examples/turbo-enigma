167476ad040e3b82ee5e7be810293fca92b660c0
Jaren Garman <jarengarman@protonmail.com>
2025-01-15 11:10:11 -0500

    feat: Add price attribute

--- lib/item.rb
+++ lib/item.rb
@@ -2,9 +2,10 @@
 
 # Create an item to stock on the food truck
 class Item
-  attr_reader :name
+  attr_reader :name, :price
 
   def initialize(item_params)
     @name = item_params[:name]
+    @price = item_params[:price].delete_prefix('$').to_f
   end
 end
