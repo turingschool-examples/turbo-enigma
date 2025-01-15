84c824ab146496f37b73f1f2ddf0ec1c98a9c0b1
Jaren Garman <jarengarman@protonmail.com>
2025-01-15 11:33:40 -0500

    feat: Add food_trucks attribute

--- lib/event.rb
+++ lib/event.rb
@@ -2,9 +2,10 @@
 
 # Create an event to host food trucks
 class Event
-  attr_reader :name
+  attr_reader :name, :food_trucks
 
   def initialize(name)
     @name = name
+    @food_trucks = []
   end
 end
