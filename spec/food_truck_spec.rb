4d35b4da73abf3bb3b7d99bf20b7eff270ab02e5
Jaren Garman <jarengarman@protonmail.com>
2025-01-15 11:17:40 -0500

    test: Add test for 0 stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -5,6 +5,9 @@ require_relative 'spec_helper'
 RSpec.describe FoodTruck do
   subject(:food_truck) { described_class.new('Rocky Mountain Pies') }
 
+  let(:first_item) { instance_double(Item, name: 'Peach Pie (Slice)', price: 3.75) }
+  let(:second_item) { instance_double(Item, name: 'Apple Pie (Slice)', price: 2.50) }
+
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
 
@@ -14,6 +17,10 @@ RSpec.describe FoodTruck do
 
     it 'has empty inventory' do
       expect(food_truck.inventory).to eq({})
+    end
+
+    it 'has no stock' do
+      expect(food_truck.check_stock(first_item)).to eq(0)
     end
   end
 end
