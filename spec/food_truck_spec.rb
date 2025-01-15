c18a8a172a23c3932901f053f901ba4351a487a2
Jaren Garman <jarengarman@protonmail.com>
2025-01-15 11:14:03 -0500

    test: Add test for name

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe FoodTruck do
 
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
+
+    it 'has a name' do
+      expect(food_truck.name).to eq('Rocky Mountain Pies')
+    end
   end
 end
