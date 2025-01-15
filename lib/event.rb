013d4a9b8b58ee83301ed4764709e062fcb8b741
Donte Handy <donte.handy@gmail.com>
2025-01-15 12:36:23 -0400

    Add Event class with methods for managing food trucks and their inventory

---
+++ lib/event.rb
@@ -0,0 +1,20 @@
+class Event
+  attr_reader :name, :food_trucks
+
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+  end
+
+  def food_truck_names
+    @food_trucks.map(&:name)
+  end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.select { |truck| truck.inventory.include?(item) }
+  end
+end
