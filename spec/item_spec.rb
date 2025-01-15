c54ed6956f1adfab6ca1d2e48106e8bd5d6a8171
Jaren Garman <jarengarman@protonmail.com>
2025-01-15 11:07:21 -0500

    test: Add test for name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe Item do
 
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
+
+    it 'has a name' do
+      expect(first_item.name).to eq('Peach Pie (Slice)')
+    end
   end
 end
