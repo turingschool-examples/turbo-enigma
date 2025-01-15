1c5281b21d96f559a6bc9e914a6eb09f8a0efbfb
Jacob Inman <jrinman6295@gmail.com>
2025-01-15 09:45:19 -0800

    potential revenue able to be calculated per truck, iteration 2 complete

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -14,4 +14,14 @@ class FoodTruck
   def stock(item, amount)
     @inventory[item] = (@inventory[item] || 0) +amount
   end
+
+  def potential_revenue
+    total_revenue = 0.0
+
+    @inventory.each do |item, amount|
+      total_revenue += item.price.delete('$').to_f * amount
+    end
+
+    total_revenue
+  end
 end
