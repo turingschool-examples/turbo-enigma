d16982dd3f8a5467aac95fc7b22b11bb0f07fb2c
Jaren Garman <jarengarman@protonmail.com>
2025-01-15 11:15:18 -0500

    feat: Add empty inventory

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -2,9 +2,10 @@
 
 # Create a food truck to hold items
 class FoodTruck
-  attr_reader :name
+  attr_reader :name, :inventory
 
   def initialize(name)
     @name = name
+    @inventory = {}
   end
 end
