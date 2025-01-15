876972b4586c5b76b6e1cec9e73c68569402404c
Sebastian McKee <sebmcke@gmail.com>
2025-01-15 10:16:07 -0600

    Add inventory attribute

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -11,6 +11,7 @@ describe FoodTruck do
         it 'exists' do
             expect(@food_truck).to be_a(FoodTruck)
             expect(@food_truck.name).to eq("Rocky Mountain Pies")
+            expect(@food_truck.inventory).to eq({})
         end
     end
 end
