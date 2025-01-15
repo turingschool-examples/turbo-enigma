32685260c8382d111e5c0f41d287473c423d1e3c
Mark Kendall <mkendall.tutor@gmail.com>
2025-01-15 10:38:24 -0600

    Add add_food_truck() and food_truck_names() methods and tests to Event class

--- lib/event.rb
+++ lib/event.rb
@@ -1,11 +1,21 @@
 class Event
   attr_reader :name, :food_trucks
-  
+
   def initialize(name)
     @name = name
 
     @food_trucks = []
 
+  end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+  end
+
+  def food_truck_names()
+    @food_trucks.map do |truck|
+      truck.name
+    end
   end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,6 +5,13 @@ require './lib/event.rb'
 RSpec.describe do
   before(:each) do
     @event = Event.new("South Pearl Street Farmers Market")
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")  
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack") 
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   it 'exists' do
@@ -14,6 +21,15 @@ RSpec.describe do
   it 'initializes correctly' do
     expect(@event.name).to eq("South Pearl Street Farmers Market")
     expect(@event.food_trucks).to eq([])
+  end
+
+  it 'can track all food trucks for an event, and list their names' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
+    expect(@event.food_truck_names()).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
 
 end
