31624a46489612f49794e391e0da0fac27891313
Jacob Inman <jrinman6295@gmail.com>
2025-01-15 09:16:10 -0800

    iter2 event spec set up, food truck pot rev method spec set up

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -36,4 +36,28 @@ RSpec.describe FoodTruck do
       expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
     end
   end
+
+  describe 'can check potential revenue' do
+    it'checks potential revenue' do
+      @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      @item10 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      @item20 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      @item30 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      @item40 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      @food_truck1.stock(@item10, 35)
+      @food_truck1.stock(@item20, 7)
+
+      @food_truck2.stock(@item40, 50)
+      @food_truck2.stock(@item30, 25)
+
+      @food_truck3.stock(@item10, 65)
+
+      expect(@food_truck1.potential_revenue).to eq(148.75)
+      expect(@food_truck2.potential_revenue).to eq(345.00)
+      expect(@food_truck3.potential_revenue).to eq(243.75)
+    end
+  end
 end
